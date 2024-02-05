3cc8da4b1045deb2a48650517d99b7b4e4a93dd8
devmarcs <devmarcelo.gus@gmail.com>
2024-02-05 00:07:18 -0300

    Criação do admin do ClientUser

--- clientuser/admin.py
+++ clientuser/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import ClientUser
 
-# Register your models here.
+
+@admin.register(ClientUser)
+class ClientUserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "cpf",
+        "auth",
+    )
+    search_fields = ("cpf",)
+    list_display_links = ("id",)
+    list_editable = (
+        "auth",
+    )
+
+
