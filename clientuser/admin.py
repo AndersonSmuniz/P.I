6a3ab9ffcafc0c64179178a3d28d3da07b46d0bd
devmarcs <devmarcelo.gus@gmail.com>
2024-02-09 11:19:18 -0300

    atualização dos admin

--- clientuser/admin.py
+++ clientuser/admin.py
@@ -5,12 +5,10 @@ from .models import ClientUser
 @admin.register(ClientUser)
 class ClientUserAdmin(admin.ModelAdmin):
     list_display = (
-        "id",
         "full_name",
         "cpf",
-        "auth",
     )
-    search_fields = ("cpf", "full_name")
-    list_display_links = ("id", "full_name")
+    search_fields = ("cpf", "full_name",)
+    list_display_links = ("full_name",)
 
 
--- service/admin.py
+++ service/admin.py
@@ -1,18 +1,21 @@
 from django.contrib import admin
 from .models import Service
+from collaborator_user.models import CollaboratorUser
 
 
 @admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
     list_display = (
-        "id",
-        "STATUS_CHOICES",
-        "salon",
-        "collaborator_user",
         "title",
         "status",
+        "salon",
+        "display_collaborator",
     )
-    search_fields = ("salon", "tittle")
-    list_display_links = ("id", "tittle")
+    search_fields = ("salon", "title")
+    list_display_links = ("title",)
     
 
+    def display_collaborator(self, obj):
+        return ", ".join([collaborator.full_name for collaborator in CollaboratorUser.objects.all()])
+
+    display_collaborator.short_description = "Collaborator"
