70741737f9cafcfbc749f08e4ccac2d859fb58d0
devmarcs <devmarcelo.gus@gmail.com>
2024-02-09 10:39:22 -0300

    criação do admin da classe service

--- clientuser/admin.py
+++ clientuser/admin.py
@@ -6,13 +6,11 @@ from .models import ClientUser
 class ClientUserAdmin(admin.ModelAdmin):
     list_display = (
         "id",
+        "full_name",
         "cpf",
         "auth",
     )
-    search_fields = ("cpf",)
-    list_display_links = ("id",)
-    list_editable = (
-        "auth",
-    )
+    search_fields = ("cpf", "full_name")
+    list_display_links = ("id", "full_name")
 
 
--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Service
 
-# Register your models here.
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "STATUS_CHOICES",
+        "salon",
+        "collaborator_user",
+        "title",
+        "status",
+    )
+    search_fields = ("salon", "tittle")
+    list_display_links = ("id", "tittle")
+    
+
