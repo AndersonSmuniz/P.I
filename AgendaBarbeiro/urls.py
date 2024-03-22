aa92e65c66f6e6d039bd2e305f8446a783ab4b2b
AndersonSmuniz <anderson.muniz100@hotmail.com>
2024-03-22 19:18:53 -0300

    Criando token e atualizacao de rotas

--- AgendaBarbeiro/urls.py
+++ AgendaBarbeiro/urls.py
@@ -3,28 +3,38 @@ from django.urls import path, include
 from rest_framework import routers
 from salon.views import SalonViewSet, LocationViewSet
 from promotion.views import PromotionListAPIView
-from schedule.views import ScheduleListAPIView
+from schedule.views import AvailableScheduleViewSet
 from booking.views import BookingViewSet
 from service.views import ServiceViewSet, ClientServiceViewSet
-from clientuser.views import ClientViewSet
+from clientuser.views import (ClientCreateViewSet, ClientUpdateViewSet, ClientDestroyViewSet)
 from collaborator_user.views import CollaboratorViewSet, SalonCollaboratorViewSet
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 router = routers.DefaultRouter()
 router.register("salon", SalonViewSet, basename="salon")
 router.register("location", LocationViewSet, basename="location")
 router.register("promotions", PromotionListAPIView, basename="promotion")
-router.register("schedules", ScheduleListAPIView, basename="schedule")
+router.register("schedules", AvailableScheduleViewSet, basename="schedule_list")
 router.register("service", ServiceViewSet, basename="service")
 router.register("client_service", ClientServiceViewSet, basename="client_service")
-router.register("client", ClientViewSet, basename="client")
 router.register("collaborator", CollaboratorViewSet, basename="collaborator")
 router.register(
     "salon_collaborator", SalonCollaboratorViewSet, basename="salon_collaborator"
 )
-router.register("create-booking", BookingViewSet, basename="create-booking"),
+router.register("create-booking", BookingViewSet, basename="create-booking")
+router.register("client/create", ClientCreateViewSet, basename="client-create")
+router.register("client/update", ClientUpdateViewSet, basename="client-update")
+router.register("client/delete", ClientDestroyViewSet, basename="client-destroy")
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     
 ]
--- schedule/views.py
+++ schedule/views.py
@@ -1,9 +1,48 @@
-from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import Schedule
 from .serializer import ScheduleSerializer
-from rest_framework import viewsets
+from datetime import datetime, timedelta
 
-class ScheduleListAPIView(viewsets.ModelViewSet):
-    queryset = Schedule.objects.all()
+def get_available_slots(day):
+    # Obtenha todos os horários disponíveis para o dia especificado
+    available_schedules = Schedule.objects.filter(day=day)
+
+    # Divida os horários disponíveis em intervalos de uma hora
+    available_slots = []
+    start_time = datetime.combine(datetime.now().date(), datetime.min.time())  # Horário inicial no dia atual
+    end_time = start_time + timedelta(hours=1)  # Próxima hora
+    for schedule in available_schedules:
+        while start_time < schedule.start:
+            available_slots.append({
+                'start': start_time,
+                'end': end_time
+            })
+            start_time += timedelta(hours=1)
+            end_time += timedelta(hours=1)
+
+        start_time = max(start_time, schedule.end)
+
+    # Adicione os horários disponíveis após o último horário de agendamento
+    while start_time < datetime.combine(datetime.now().date(), datetime.max.time()):
+        available_slots.append({
+            'start': start_time,
+            'end': start_time + timedelta(hours=1)
+        })
+        start_time += timedelta(hours=1)
+
+    return available_slots
+
+class AvailableScheduleViewSet(viewsets.ViewSet):
     serializer_class = ScheduleSerializer
+
+    @action(detail=False, methods=['get'])
+    def list_available_slots(self, request):
+        # Obtenha o dia da semana atual (0 para segunda-feira, 6 para domingo)
+        current_day = datetime.now().weekday() + 1
+
+        # Obtenha os horários disponíveis para o dia atual
+        available_slots = get_available_slots(current_day)
+
+        return Response(available_slots)
