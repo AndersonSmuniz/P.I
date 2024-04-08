8e0349ee0c2837f768743a8e8443f39b636d474c
AndersonSmuniz <anderson.muniz100@hotmail.com>
2024-04-08 20:01:50 -0300

    Att

--- AgendaBarbeiro/urls.py
+++ AgendaBarbeiro/urls.py
@@ -5,13 +5,22 @@ from salon.views import SalonViewSet, LocationViewSet
 from promotion.views import PromotionListAPIView
 from schedule.views import AvailableScheduleViewSet
 from booking.views import BookingViewSet
-from service.views import ServiceViewSet, ClientServiceViewSet, SalonServicesView
-from clientuser.views import (ClientCreateViewSet, ClientUpdateViewSet, ClientDestroyViewSet)
+from service.views import (
+    ServiceViewSet,
+    ClientServiceViewSet,
+    SalonServicesView,
+    ServiceCollaboratorsListView,
+)
+from clientuser.views import (
+    ClientCreateViewSet,
+    ClientUpdateViewSet,
+    ClientDestroyViewSet,
+)
 from collaborator_user.views import CollaboratorViewSet, SalonCollaboratorViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
-    TokenVerifyView
+    TokenVerifyView,
 )
 
 router = routers.DefaultRouter()
@@ -33,10 +42,13 @@ router.register("client/delete", ClientDestroyViewSet, basename="client-destroy"
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(router.urls)),
-    path('salon/<int:id>/services', SalonServicesView.as_view(), name="salon_services"),
-
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    
+    path("salon/<int:id>/services", SalonServicesView.as_view(), name="salon_services"),
+    path(
+        "service/<int:service_id>/collaborators/",
+        ServiceCollaboratorsListView.as_view(),
+        name="service_collaborators_list",
+    ),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
--- service/views.py
+++ service/views.py
@@ -1,10 +1,11 @@
 from rest_framework import viewsets, status
 from rest_framework.generics import ListCreateAPIView
 from .serializer import ServiceSerializer
-from .models import Service, ClientService
+from .models import Service, ClientService, CollaboratorService
 from .serializer import (
     ServiceSerializer,
     ClientServiceSerializer,
+    CollaboratorServiceSerializer
 )
 
 
@@ -17,16 +18,28 @@ class ClientServiceViewSet(viewsets.ModelViewSet):
     queryset = ClientService.objects.all()
     serializer_class = ClientServiceSerializer
 
+
 class SalonServicesView(ListCreateAPIView):
-    queryset = Service.objects.all()  # Define o queryset para todos os serviços
-    serializer_class = ServiceSerializer  # Define o serializer a ser usado
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
 
     def get_queryset(self):
-        # Filtra o queryset para retornar apenas os serviços relacionados ao salão específico
-        salon_id = self.kwargs.get('id')
+
+        salon_id = self.kwargs.get("id")
         return self.queryset.filter(salon_id=salon_id)
 
     def perform_create(self, serializer):
-        # Define o salão associado ao serviço antes de salvar
-        salon_id = self.kwargs.get('salon_id')
+
+        salon_id = self.kwargs.get("salon_id")
         serializer.save(salon_id=salon_id)
+
+
+class ServiceCollaboratorsListView(ListCreateAPIView):
+    queryset = CollaboratorService.objects.all()
+    serializer_class = CollaboratorServiceSerializer
+
+    def get_queryset(self):
+        service_id = self.kwargs.get("service_id")
+        return CollaboratorService.objects.filter(
+            service_id=service_id, status="active"
+        )
